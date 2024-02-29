ef640e9c49adfb6ebefb4b8851033fafa469f515
nabilkh55 <nabilkholafi5@gmail.com>
2024-02-29 21:07:23 +0700

    first push

---
+++ js/script.js
@@ -0,0 +1,23 @@
+function validateForm() {
+    const date = Date();
+    const name = document.forms["message-form"]["full-name"].value;
+    const birthDate = document.forms["message-form"]["birth-date"].value;
+    const gender = document.forms["message-form"]["gender"].value;
+    const messages = document.forms["message-form"]["messages"].value;
+
+    if (name == "" || birthDate == "" || gender == "" || messages == "") {
+        alert("Tidak boleh ada yang kosong");
+        return false;
+    }
+
+    setSenderUI(date, name, birthDate, gender, messages);
+    return false;
+}
+
+function setSenderUI(date, name, birthDate, gender, messages) {
+    document.getElementById("sender-current-time").innerHTML = date;
+    document.getElementById("sender-full-name").innerHTML = name;
+    document.getElementById("sender-birth-date").innerHTML = birthDate;
+    document.getElementById("sender-gender").innerHTML = gender;
+    document.getElementById("sender-messages").innerHTML = messages;
+}
